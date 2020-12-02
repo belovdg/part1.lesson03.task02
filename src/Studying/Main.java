1199b65af48a572cdb12e87a23cd4dcd64795738
belovdg <belovdg87@yandex.ru>
2020-12-02 13:50:29 +0300

    Initial commit

---
+++ src/Studying/Main.java
@@ -0,0 +1,11 @@
+package Studying;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int zp = 70000;
+        double nalog = 0.87;
+        System.out.println(zp*nalog);
+    }
+}
+
