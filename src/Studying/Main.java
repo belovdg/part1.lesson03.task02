871a23210ecf3fb7b21266d539ad728abc205401
belovdg <belovdg87@yandex.ru>
2020-12-06 19:42:40 +0300

    Изменение

--- src/Studying/Main.java
+++ src/Studying/Main.java
@@ -3,9 +3,13 @@ package Studying;
 public class Main {
 
     public static void main(String[] args) {
-        int zp = 70000;
+        double salary = 70000;
         double nalog = 0.87;
-        System.out.println(zp*nalog);
+        double net = salary*nalog;
+        if (salary>0)
+            System.out.println("Выдача зарплаты на руки " + net + " руб.");
+        else System.out.println("Ошибка подсчета зарплаты");
     }
 }
+
 
